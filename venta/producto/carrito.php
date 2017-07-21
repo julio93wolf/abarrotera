ae15b054cb4228ae3d36e3d17650c0e32d85f63e
Julio Rodriguez <juliovr_93@hotmail.com>
2017-07-21 01:49:50 -0500

    Carrito

--- venta/producto/carrito.php
+++ venta/producto/carrito.php
@@ -11,12 +11,17 @@
 		}
 		foreach ($_POST['carrito'] as $key => $value) {
 			if(!empty($value)){
-				$carrito[$key]['cantidad']= (isset($carrito[$key])) ?
-				$paraPresentacion['sku']=$key;
-				$datoPresentacion=$abarrotera->consultar('select * from vw_productos where sku=:sku',$paraPresentacion);
-				$carrito[$key]['precio_unitario']=$datoPresentacion[0]['preciou_descuento'];
+				$carrito[$key]['cantidad'] = (isset($carrito[$key])) ? $carrito[$key]['cantidad']+$value : $value;
+				$paraProducto['sku']=$key;
+				$datoPresentacion=$abarrotera->consultar('select * from vw_productos where sku=:sku',$paraProducto);
+				if($value>=$datos[0]['cantidadm']){
+					$carrito[$key]['precio_unitario'] = $datos[0]['preciom_descuento'];  
+				}
+				else{
+					$carrito[$key]['precio_unitario'] = $datos[0]['preciou_descuento'];
+				}
 				$carrito[$key]['descuento_aplicado']=$datoPresentacion[0]['descuento'];
-				$carrito[$key]['subtotal']=$value*$datoPresentacion[0]['preciou_descuento'];
+				$carrito[$key]['subtotal']=$carrito[$key]['cantidad'] * $carrito[$key]['precio_unitario'];
 			}
 		}
 		$_SESSION['carrito']=$carrito;
